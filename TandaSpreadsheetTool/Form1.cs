25c5fbeb7ededa322a32f9442a4e9a53f8761d8a
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2018-06-18 15:33:15 +0100

    added json library

--- TandaSpreadsheetTool/Form1.cs
+++ TandaSpreadsheetTool/Form1.cs
@@ -12,9 +12,16 @@ namespace TandaSpreadsheetTool
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ TandaSpreadsheetTool/Networker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TandaSpreadsheetTool
+{
+    class Networker
+    {
+        private string token = "f2b3c50dfd16d7c69d538ea20cfba702aa38c6762d86a51a643bbfd64ef36017";
+
+        private string userName = "as@esmsglobal.com";
+
+
+    }
+}
